80603b2f2d838e4b73975c0d4128f2806e9f0694
velafarq <vela.mrdjen@gmail.com>
2018-06-05 10:32:58 -0500

    Fix DATABASE_URL typo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABSE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://velafarq:Tafthenry22@ds249530.mlab.com:49530/blog-app-db';
 exports.PORT = process.env.PORT || 8080;
